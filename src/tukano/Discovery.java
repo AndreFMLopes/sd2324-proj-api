ed70109527729f42069ee9f3486b4b72b3ea0aad
aflop <afm.lopes@campus.fct.unl.pt>
2024-04-15 20:22:11 +0100

    Added multicast Discovery system

---
+++ src/tukano/Discovery.java
@@ -0,0 +1,173 @@
+package tukano;
+
+import java.net.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * <p>A class interface to perform service discovery based on periodic 
+ * announcements over multicast communication.</p>
+ * 
+ */
+
+public interface Discovery {
+
+	/**
+	 * Used to announce the URI of the given service name.
+	 * @param serviceName - the name of the service
+	 * @param serviceURI - the uri of the service
+	 */
+	public void announce(String serviceName, String serviceURI);
+
+	/**
+	 * Get discovered URIs for a given service name
+	 * @param serviceName - name of the service
+	 * @param minReplies - minimum number of requested URIs. Blocks until the number is satisfied.
+	 * @return array with the discovered URIs for the given service name.
+	 */
+	public URI[] knownUrisOf(String serviceName, int minReplies) throws InterruptedException;
+
+	/**
+	 * Get the instance of the Discovery service
+	 * @return the singleton instance of the Discovery service
+	 */
+	public static Discovery getInstance() {
+		return DiscoveryImpl.getInstance();
+	}
+}
+
+/**
+ * Implementation of the multicast discovery service
+ */
+class DiscoveryImpl implements Discovery {
+	
+	private static Logger Log = Logger.getLogger(Discovery.class.getName());
+
+	// The pre-aggreed multicast endpoint assigned to perform discovery.
+
+	static final int DISCOVERY_RETRY_TIMEOUT = 5000;
+	static final int DISCOVERY_ANNOUNCE_PERIOD = 10000;
+
+	// Replace with appropriate values: allowed IP Multicast range: 224.0.0.1 - 239.255.255.255
+	static final InetSocketAddress DISCOVERY_ADDR = new InetSocketAddress("224.0.0.1", 12345);
+
+	// Used to separate the two fields that make up a service announcement.
+	private static final String DELIMITER = "\t";
+
+	private static final int MAX_DATAGRAM_SIZE = 65536;
+
+	private static Discovery singleton;
+
+	synchronized static Discovery getInstance() {
+		if (singleton == null) {
+			singleton = new DiscoveryImpl();
+		}
+		return singleton;
+	}
+
+	private DiscoveryImpl() {
+		this.startListener();
+	}
+
+	@Override
+	public void announce(String serviceName, String serviceURI) {
+		Log.info(String.format("Starting Discovery announcements on: %s for: %s -> %s\n", DISCOVERY_ADDR, serviceName,
+				serviceURI));
+
+		var pktBytes = String.format("%s%s%s", serviceName, DELIMITER, serviceURI).getBytes();
+		var pkt = new DatagramPacket(pktBytes, pktBytes.length, DISCOVERY_ADDR);
+
+		// start thread to send periodic announcements
+		new Thread(() -> {
+			try (var ds = new DatagramSocket()) {
+				while (true) {
+					try {
+						ds.send(pkt);
+						Thread.sleep(DISCOVERY_ANNOUNCE_PERIOD);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}).start();
+	}
+
+	static Map<String, URI[]> knownUris = new HashMap<>();
+
+	@Override
+	public URI[] knownUrisOf(String serviceName, int minEntries) throws InterruptedException {
+		URI[] result = null;
+		for (int i = 0; i < 5; i++) {
+			result = knownUris.get(serviceName);
+
+			if (result != null && result.length >= minEntries) {
+				return result;
+			} else {
+				Thread.sleep(DISCOVERY_RETRY_TIMEOUT);
+			}
+		}
+		return result;
+	}
+
+	private void startListener() {
+		Log.info(String.format("Starting discovery on multicast group: %s, port: %d\n", DISCOVERY_ADDR.getAddress(),
+				DISCOVERY_ADDR.getPort()));
+
+		new Thread(() -> {
+			try (var ms = new MulticastSocket(DISCOVERY_ADDR.getPort())) {
+				ms.joinGroup(DISCOVERY_ADDR, NetworkInterface.getByInetAddress(InetAddress.getLocalHost()));
+				for (;;) {
+					try {
+						var pkt = new DatagramPacket(new byte[MAX_DATAGRAM_SIZE], MAX_DATAGRAM_SIZE);
+						ms.receive(pkt);
+
+						var msg = new String(pkt.getData(), 0, pkt.getLength());
+
+						var parts = msg.split(DELIMITER);
+						if (parts.length == 2) {
+							// TODO: complete by storing the decoded announcements...
+							var serviceName = parts[0];
+							var uri = URI.create(parts[1]);
+
+							URI[] uris = knownUris.get(serviceName);
+							if (uris == null){
+								uris = new URI[1];
+							} else if (uris[uris.length - 1] != null) {
+								uris = resize(uris);
+							}
+
+							for (int i = 0; i < uris.length; i++) {
+								if (uris[i] == null) {
+									uris[i] = uri;
+									knownUris.put(serviceName, uris);
+									break;
+								} else if (uris[i].equals(uri)) {
+									break;
+								}
+							}
+						}
+
+//					    for (Map.Entry<String, URI[]> entry: knownUris.entrySet()) {
+//						    System.out.println(String.format("Known URIs for %s", entry.getKey()));
+//						    for (URI uri: entry.getValue()) {
+//							    System.out.println(uri.toString());
+//						    }
+//						}
+					} catch (Exception x) {
+						x.printStackTrace();
+					}
+				}
+			} catch (Exception x) {
+				x.printStackTrace();
+			}
+		}).start();
+	}
+
+	private URI[] resize(URI[] arr) {
+		return Arrays.copyOf(arr, arr.length + 1);
+	}
+}
