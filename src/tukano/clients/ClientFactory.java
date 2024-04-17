87c81b82638e6286e45cc03daff02d4d6dd5dece
Leonardo <LeonardoGGC21@gmail.com>
2024-04-17 19:32:22 +0100

    improved

--- src/tukano/clients/ClientFactory.java
+++ src/tukano/clients/ClientFactory.java
@@ -4,27 +4,31 @@ import java.net.URI;
 
 import tukano.Discovery;
 import tukano.api.java.*;
+import tukano.api.java.Result.ErrorCode;
 import tukano.clients.rest.*;
 import tukano.impl.grpc.clients.*;
 
 public class ClientFactory {
 	
-	public static Users getUsersClient() throws InterruptedException {
-		URI[] userServers = Discovery.getInstance().knownUrisOf("users", 1);
-		if(userServers[0].toString().endsWith("rest")) return new RestUsersClient(userServers[0]);
-		return new GrpcUsersClient(userServers[0]);
+	public static Result<Users> getUsersClient() throws InterruptedException {
+		URI[] usersServers = Discovery.getInstance().knownUrisOf("users", 1);
+		if(usersServers.length == 0)return Result.error(ErrorCode.NOT_FOUND);
+		if(usersServers[0].toString().endsWith("rest")) return Result.ok(new RestUsersClient(usersServers[0]));
+		return Result.ok(new GrpcUsersClient(usersServers[0]));
 	}
 	
-	public static Shorts getShortsClient() throws InterruptedException {
-		URI[] userServers = Discovery.getInstance().knownUrisOf("shorts", 1);
-		if(userServers[0].toString().endsWith("rest")) return new RestShortsClient(userServers[0]);
-		return new GrpcShortsClient(userServers[0]);
+	public static Result<Shorts> getShortsClient() throws InterruptedException {
+		URI[] shortsServers = Discovery.getInstance().knownUrisOf("shorts", 1);
+		if(shortsServers.length == 0)return Result.error(ErrorCode.NOT_FOUND);
+		if(shortsServers[0].toString().endsWith("rest")) return Result.ok(new RestShortsClient(shortsServers[0]));
+		return Result.ok(new GrpcShortsClient(shortsServers[0]));
 	}
 	
-	public static Blobs getBlobsClient() throws InterruptedException {
-		URI[] userServers = Discovery.getInstance().knownUrisOf("blobs", 1);
-		if(userServers[0].toString().endsWith("rest")) return new RestBlobsClient(userServers[0]);
-		return new GrpcBlobsClient(userServers[0]);
+	public static Result<Blobs> getBlobsClient() throws InterruptedException {
+		URI[] blobsServers = Discovery.getInstance().knownUrisOf("blobs", 1);
+		if(blobsServers.length == 0)return Result.error(ErrorCode.NOT_FOUND);
+		if(blobsServers[0].toString().endsWith("rest")) return Result.ok(new RestBlobsClient(blobsServers[0]));
+		return Result.ok(new GrpcBlobsClient(blobsServers[0]));
 	}
 
 }
